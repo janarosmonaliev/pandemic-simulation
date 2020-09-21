d12737caa48ac831466ad786cd7c25e5bffd9a0e
Zhanarbek Osmonaliev <janar.osmonaliev@gmail.com>
2020-09-21 22:46:52 +0900

    Styling updated.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var map = L.map("map");
 
 var tangramLayer = Tangram.leafletLayer({
-  scene: "scene.yaml",
+  scene: "scene-alt.yaml",
   attribution:
     '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors',
 });
