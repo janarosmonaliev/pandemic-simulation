f8e1f30eb11afd865d0f311da1d05a4a0f3368f8
Zhanarbek Osmonaliev <janar.osmonaliev@gmail.com>
2020-09-21 19:48:20 +0900

    Migration to Tangram.js from Harp.gl

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
-const canvas = document.getElementById("map");
-const map = new harp.MapView({
-  canvas,
-  theme:
-    "https://unpkg.com/@here/harp-map-theme@latest/resources/berlin_tilezen_night_reduced.json",
-  //For tile cache optimization:
-  maxVisibleDataSourceTiles: 100,
-  tileCacheSize: 200,
+var map = L.map("map");
+
+var tangramLayer = Tangram.leafletLayer({
+  scene: "scene.yaml",
+  attribution:
+    '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors',
 });
 
-map.setCameraGeolocationAndZoom(
-  new harp.GeoCoordinates(1.278676, 103.850216),
-  16
-);
+tangramLayer.addTo(map);
 
-const mapControls = new harp.MapControls(map);
-const ui = new harp.MapControlsUI(mapControls);
-canvas.parentElement.appendChild(ui.domElement);
-
-mapControls.maxPitchAngle = 90;
-// mapControls.setRotation(6.3, 50);
-
-map.resize(window.innerWidth, window.innerHeight);
-window.onresize = () => map.resize(window.innerWidth, window.innerHeight);
-
-const omvDataSource = new harp.OmvDataSource({
-  baseUrl: "https://xyz.api.here.com/tiles/herebase.02",
-  apiFormat: harp.APIFormat.XYZOMV,
-  styleSetName: "tilezen",
-  authenticationCode: "AJ2-vU60Q_6dpaTrEQjOHgA",
-});
-map.addDataSource(omvDataSource);
-// J0IJdYzKDYS3nHVDDEWETIqK3nAcxqW42vz7xeSq61M
-// AJ2-vU60Q_6dpaTrEQjOHgA
+map.setView([37.3839, 126.6439], 16);
