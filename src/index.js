54d872a38006979414af335b63317587a713576f
Zhanarbek Osmonaliev <zhanarbek.osmonaliev@stonybrook.edu>
2021-01-31 01:18:36 +0900

    Project finalized

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import { Matrix4, Vector3 } from "three";
 import * as dat from "dat.gui";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import Flat from "./objects/Flat.gltf";
+import Flat2 from "./objects/Flat2.gltf";
 import FlatImg from "./objects/HouseTexture1.png";
+import House from "./objects/House.gltf";
+import HouseImg from "./objects/HouseTexture1.png";
+
+// Global variables
 var renderer, camera, scene, controls;
 var plane, cube;
 var cityWidth = 1300;
@@ -49,7 +54,7 @@ function init() {
   document.querySelector(".counter").innerHTML = control.infectedBlobs;
   // SECTION: Renderer
   renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setClearColor(new THREE.Color(0xb0b0b0));
+  renderer.setClearColor(new THREE.Color(0xffffff));
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.shadowMap.enabled = true;
   // invalidation.then(() => renderer.dispose());
@@ -59,12 +64,14 @@ function init() {
   // SECTION: Scene & Camera
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(
-    75,
+    60,
     window.innerWidth / window.innerHeight,
     0.1,
-    15000
+    10000
   );
-  // camera.position.set(1000, 50, 1500);
+  camera.position.y = 1300;
+  camera.position.z = 0;
+  camera.position.x = -600;
   scene.add(camera);
 
   // SECTION: Controls
@@ -72,21 +79,20 @@ function init() {
   controls.enableDamping = true;
   controls.dampingFactor = 0.05;
   controls.screenSpacePanning = false;
-
   controls.maxPolarAngle = Math.PI * 0.45;
   controls.minPolarAngle = Math.PI * 0.0;
-  controls.minDistance = 50;
-  controls.maxDistance = 5000;
+  controls.minDistance = 200;
+  controls.maxDistance = 1500;
 
   // NOTE: Skybox
   var skyBoxGeometry = new THREE.CubeGeometry(10000, 10000, 10000);
   var skyBoxMaterial = new THREE.MeshBasicMaterial({
-    color: 0xf2f2f2,
+    color: 0xffffff,
     side: THREE.BackSide,
   });
   var skyBox = new THREE.Mesh(skyBoxGeometry, skyBoxMaterial);
   scene.add(skyBox);
-  scene.fog = new THREE.Fog(skyBoxMaterial.color, 1000, 4000);
+  scene.fog = new THREE.Fog(skyBoxMaterial.color, 300, 4000);
 
   // NOTE: Plane
   var planeGeometry = new THREE.PlaneBufferGeometry(1300, 1300);
@@ -103,14 +109,24 @@ function init() {
   plane.rotation.x = -Math.PI / 2;
   scene.add(plane);
 
+  var scenePlaneGeometry = new THREE.PlaneBufferGeometry(10000, 10000);
+  var scenePlaneMaterial = new THREE.MeshPhongMaterial({
+    color: 0xf3f3f3,
+    side: THREE.DoubleSide,
+  });
+  var scenePlane = new THREE.Mesh(scenePlaneGeometry, scenePlaneMaterial);
+  scenePlane.rotation.x = -Math.PI / 2;
+  scenePlane.position.y = -15;
+  scene.add(scenePlane);
+
   // NOTE Light
-  var ambientLight = new THREE.AmbientLight(0x787878, 0.7);
+  var ambientLight = new THREE.AmbientLight(0x787878, 0.9);
   scene.add(ambientLight);
 
   var light = new THREE.DirectionalLight(0xffffff, 1);
   light.position.set(-500, 1000, -300);
 
-  light.shadow.camera.near = 50;
+  light.shadow.camera.near = 2;
   light.shadow.camera.far = 2000;
   light.shadow.camera.left = -2000;
   light.shadow.camera.right = 2000;
@@ -134,13 +150,11 @@ function init() {
   // SECTION City Generation
   scene.add(generateCity(cityWidth, cityLength));
 
-  camera.position.y = 1300;
-  camera.position.z = 600;
-  camera.position.x = 0;
-
+  // Adjust the scene on window resize
   window.addEventListener("resize", onWindowResize, false);
 }
 
+// Remove blobs from the scene
 function resetBlobs() {
   pathsArray.forEach((e) => {
     e.sphere.parent.remove(e.sphere);
@@ -149,9 +163,9 @@ function resetBlobs() {
   pathsArray = [];
 }
 
+// Animation
 function animate() {
   if (!control.play) return;
-
   requestAnimationFrame(animate);
   controls.update();
   render();
@@ -163,279 +177,6 @@ function onWindowResize() {
 
   renderer.setSize(window.innerWidth, window.innerHeight);
   // effect.setSize(window.innerWidth, window.innerHeight);
-}
-
-// function generateCity(cityWidth, cityLength) {
-//   var districtMinSize = 50;
-//   var gridWidth = Math.floor(cityWidth / districtMinSize);
-//   var gridLength = Math.floor(cityLength / districtMinSize);
-//   var cityGrid = generateCityGrid(gridWidth, gridLength, districtMinSize);
-
-//   // Creating districts
-//   var cityGeometry = new THREE.Geometry();
-//   var value = 0;
-
-//   for (var i = 0; i < gridWidth; i++) {
-//     for (var j = 0; j < gridLength; j++) {
-//       if (cityGrid[i][j] == value) {
-//         for (var k = i; k < gridWidth; k++) {
-//           if (cityGrid[k][j] != value) {
-//             break;
-//           }
-//         }
-//         var districtWidth = k - i;
-
-//         for (var k = j + 1; k < gridLength; k++) {
-//           if (cityGrid[i][k] != value) {
-//             break;
-//           }
-//         }
-//         var districtLength = k - j;
-
-//         for (var k = 0; k < districtWidth; k++) {
-//           for (var l = 0; l < districtLength; l++) {
-//             cityGrid[k + i][l + j] = 1;
-//           }
-//         }
-
-//         districtWidth *= districtMinSize;
-//         districtLength *= districtMinSize;
-
-//         var districtHeight = generateDistrictHeightRange();
-
-//         // console.log("District. Width: " + districtWidth + " Length " + districtLength)
-//         var district = generateDistrict(
-//           districtWidth,
-//           districtLength,
-//           districtHeight.x,
-//           districtHeight.y
-//         );
-//         district.applyMatrix4(
-//           new THREE.Matrix4().makeTranslation(
-//             i * districtMinSize,
-//             0,
-//             j * districtMinSize
-//           )
-//         );
-
-//         // NOTE Deprecated function
-//         THREE.GeometryUtils.merge(cityGeometry, district);
-//         // cityGeometry.merge(district);
-//       }
-//     }
-//   }
-
-//   var material = new THREE.MeshLambertMaterial({
-//     vertexColors: THREE.VertexColors,
-//   });
-//   var mesh = new THREE.Mesh(cityGeometry, material);
-//   mesh.castShadow = true;
-//   mesh.receiveShadow = true;
-
-//   mesh.applyMatrix4(
-//     new THREE.Matrix4().makeTranslation(-cityWidth / 2, 0, -cityLength / 2)
-//   );
-//   mesh.matrixAutoUpdate = false;
-//   return mesh;
-// }
-
-function generateCityGrid(gridWidth, gridLength, districtMinSize) {
-  var cityGrid = new Array();
-
-  // Initialize city grid as an Array
-  for (var i = 0; i < gridWidth; i++) {
-    cityGrid[i] = new Array();
-    for (var j = 0; j < gridLength; j++) {
-      cityGrid[i][j] = 0;
-    }
-  }
-  var districtScale = 4;
-
-  // Horizontal Routes
-  for (var i = 0; i < gridWidth - 0; i += districtScale + 1) {
-    for (var j = 0; j < gridLength - 0; j++) {
-      cityGrid[i][j] = 1;
-    }
-  }
-
-  // Vertical Routes
-  for (var i = 0; i < gridWidth - 0; i += districtScale + 1) {
-    for (var j = 0; j < gridLength - 0; j++) {
-      cityGrid[j][i] = 1;
-    }
-  }
-  // NOTE IDK Why I am doing this.
-
-  return cityGrid;
-}
-
-function generateDistrictHeightRange() {
-  var ranges = new Array();
-  var i = 0;
-  // ranges[i++] = new THREE.Vector2(30, 60); // Very small
-  // ranges[i++] = new THREE.Vector2(50, 90); // Small
-  ranges[i++] = new THREE.Vector2(80, 130); // Medium
-  ranges[i++] = new THREE.Vector2(100, 200); // Medium-tall
-  ranges[i++] = new THREE.Vector2(90, 250); // Tall
-
-  return ranges[randomInt(0, i)];
-}
-
-function generateDistrict(width, length, minHeight, maxHeight) {
-  var districtGeometry = new THREE.Geometry();
-  var rowWidth = 0;
-  var rowLength = 0;
-  var totalLength = 0;
-
-  for (var j = 0; j < length; j += 5) {
-    if (length - j < 45) break;
-
-    var rowGeometry = new THREE.Geometry();
-    for (var i = 0; i < width; i += 5) {
-      if (width - i < 45) break;
-      var building = generateBuilding(length - j, minHeight, maxHeight);
-
-      var floorWidth = Math.floor(80 / 5) * 5 + 5;
-      var floorLength = Math.floor(80 / 5) * 5 + 5;
-      if (floorLength > rowLength) rowLength = floorLength;
-      building.Mesh.position.x = i + floorWidth / 2.0;
-      building.Mesh.position.z = j + rowLength / 2.0;
-      // NOTE Deprecated
-      THREE.GeometryUtils.merge(rowGeometry, building.Mesh);
-
-      i += floorWidth;
-      rowWidth = i;
-    }
-
-    rowGeometry.applyMatrix4(
-      new THREE.Matrix4().makeTranslation((width - rowWidth) / 2.0, 0, 0)
-    );
-    // NOTE Deprecated
-    THREE.GeometryUtils.merge(districtGeometry, rowGeometry);
-    // districtGeometry.merge(rowGeometry);
-
-    j += rowLength;
-    totalLength = j;
-
-    rowWidth = 0;
-    rowLength = 0;
-  }
-
-  districtGeometry.applyMatrix4(
-    new Matrix4().makeTranslation(0, 0, (length - totalLength) / 2.0)
-  );
-
-  var planeGeometry = new THREE.PlaneGeometry(width, length);
-  var planeMaterial = new THREE.MeshLambertMaterial();
-  var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-
-  var planeColor = new THREE.Color(0xc8c8c8);
-  plane.geometry.faces[0].vertexColors = [planeColor, planeColor, planeColor];
-  plane.geometry.faces[1].vertexColors = [planeColor, planeColor, planeColor];
-
-  plane.rotation.x = -0.5 * Math.PI;
-  plane.position.x = width / 2;
-  plane.position.y = 1;
-  plane.position.z = length / 2;
-
-  // NOTE Deprecated
-  THREE.GeometryUtils.merge(districtGeometry, plane);
-  // districtGeometry.merge(plane);
-
-  var material = new THREE.MeshLambertMaterial({
-    vertexColors: THREE.VertexColors,
-  });
-  var mesh = new THREE.Mesh(districtGeometry, material);
-  mesh.castShadow = true;
-  mesh.receiveShadow = true;
-
-  return mesh;
-}
-
-function generateBuilding(bldgMaxWidth, bldgMinHeight, bldgMaxHeight) {
-  var minWidth = 80;
-  var maxWidth = 80;
-
-  bldgMaxWidth =
-    bldgMaxWidth < minWidth
-      ? minWidth
-      : bldgMaxWidth > maxWidth
-      ? maxWidth
-      : bldgMaxWidth;
-
-  var width = random(minWidth, bldgMaxWidth);
-  var height = random(bldgMinHeight, bldgMaxHeight);
-  var length = width;
-
-  // Building creation
-  var geometry = new THREE.BoxGeometry(width, height, length);
-  geometry.applyMatrix4(new THREE.Matrix4().makeTranslation(0, height / 2, 0));
-
-  var material = new THREE.MeshLambertMaterial({
-    color: 0xffffff,
-    vertexColors: THREE.VertexColors,
-  });
-  var mesh = new THREE.Mesh(geometry, material);
-  mesh.castShadow = true;
-  mesh.receiveShadow = true;
-
-  var light = new THREE.Color(0xe8e8e8); // 0xFFFFFF
-  var shadow = new THREE.Color(0x606060); // 0x707070
-
-  var value = 1 - Math.random() * Math.random() * Math.random();
-  var fade = 1.5; // 0x3DA2EF
-  var baseColor = new THREE.Color().setRGB(
-    value /*+ Math.random()*/ * fade,
-    value /*+ Math.random()*/ * fade,
-    value /*+ Math.random()*/ * fade
-  );
-  var topColor = baseColor.clone().multiply(light);
-  var bottomColor = baseColor.clone().multiply(shadow);
-
-  // var random_building_color = Math.random() * Math.random() * Math.random();
-
-  // if (random_building_color > 0.65) {
-  //   baseColor = new THREE.Color().setRGB(0.9, 0.2, 0.2);
-  //   topColor = baseColor.clone().multiply(light);
-  //   bottomColor = baseColor.clone().multiply(shadow);
-  // }
-
-  var faces = mesh.geometry.faces;
-  var i = 0;
-
-  // Front face
-  faces[i++].vertexColors = [topColor, bottomColor, topColor];
-  faces[i++].vertexColors = [bottomColor, bottomColor, topColor];
-
-  // Back face
-  faces[i++].vertexColors = [topColor, bottomColor, topColor];
-  faces[i++].vertexColors = [bottomColor, bottomColor, topColor];
-
-  // Top face
-  faces[i++].vertexColors = [baseColor, baseColor, baseColor];
-  faces[i++].vertexColors = [baseColor, baseColor, baseColor];
-
-  // Bottom face
-  i += 2;
-
-  // Left face
-  faces[i++].vertexColors = [topColor, bottomColor, topColor];
-  faces[i++].vertexColors = [bottomColor, bottomColor, topColor];
-
-  // Right face
-  faces[i++].vertexColors = [topColor, bottomColor, topColor];
-  faces[i++].vertexColors = [bottomColor, bottomColor, topColor];
-
-  mesh.geometry.faces = faces;
-
-  return new Building(width, height, length, mesh);
-}
-
-function Building(Width, Height, Length, Mesh) {
-  this.Width = Width;
-  this.Height = Height;
-  this.Length = Length;
-  this.Mesh = Mesh;
 }
 
 // SECTION Generate City
@@ -480,12 +221,21 @@ function generateCity(cityWidth, cityLength) {
           0,
           yOffset + yShift
         );
-        generateFlat(matrix, isHouseLeft(k));
+        var bldgType = random(0, 100);
+        if (bldgType <= 33) {
+          generateFlat(Flat, FlatImg, matrix, isHouseLeft(k), 30);
+        } else if (bldgType > 33 && bldgType <= 60) {
+          generateFlat(Flat2, FlatImg, matrix, isHouseLeft(k), 30);
+        } else {
+          generateFlat(House, HouseImg, matrix, isHouseLeft(k), 60);
+        }
       }
     }
   }
 }
-function generateFlat(matrix, rotateLeft) {
+
+// SECTION Load GLTF Object and add it to the scene
+function generateFlat(GLTFObject, objTexture, matrix, rotateLeft, scale) {
   var matrixGlobal = new THREE.Matrix4().makeTranslation(
     -cityWidth / 2,
     0,
@@ -498,10 +248,10 @@ function generateFlat(matrix, rotateLeft) {
     color: 0xffffff,
   });
   var textureLoader = new THREE.TextureLoader();
-  var texture = textureLoader.load(FlatImg);
+  var texture = textureLoader.load(objTexture);
   var rotation = rotateLeft ? -Math.PI / 2 : Math.PI / 2;
   loader.load(
-    Flat,
+    GLTFObject,
     function (gltf) {
       var flat = gltf.scene.children[0];
 
@@ -520,7 +270,7 @@ function generateFlat(matrix, rotateLeft) {
       flat.position.y = 1;
       flat.rotateY(rotation);
       // flat.updateMatrix();
-      flat.scale.set(30, 30, 30);
+      flat.scale.set(scale, scale, scale);
       scene.add(flat);
     },
     undefined,
@@ -530,10 +280,11 @@ function generateFlat(matrix, rotateLeft) {
   );
 }
 
+// Random float
 function random(min, max) {
   return min + Math.random() * (max - min);
 }
-
+// Random Integer
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -554,6 +305,8 @@ function generatePopulation() {
     pathsArray[i].isInfected = true;
   }
 }
+
+// Generate path for each blob
 function generatePath(src, dst) {
   var matrix = new THREE.Matrix4().makeTranslation(
     -cityWidth / 2,
@@ -617,10 +370,11 @@ function generatePath(src, dst) {
   });
 }
 
+// Create a blob
 function generateBlob() {
-  var geometry = new THREE.SphereGeometry(8, 32, 32);
+  var geometry = new THREE.SphereGeometry(6, 20, 20);
   var material = new THREE.MeshBasicMaterial({
-    color: 0x3792cb,
+    color: 0x0dae62,
     transparent: true,
     opacity: 0.9,
   });
@@ -631,23 +385,27 @@ function generateBlob() {
   return sphere;
 }
 
+// Get a random house's indexes
 function randomHouse() {
   return [
     Math.floor(Math.random() * Math.floor(25)) + 1,
     Math.floor(Math.random() * Math.floor(4)) + 1,
   ];
 }
+// Whether house is adjacent to the left road in district
 function isHouseLeft(index) {
   if (index % 2 == 1) {
     return true;
   } else return false;
 }
+// Whether house is in the upper location within district
 function isHouseUpper(index) {
   if (index <= 2) {
     return true;
   } else return false;
 }
 
+// SECTION Rendering logic
 function render() {
   pathsArray.forEach((e) => {
     var newPos = e.points.getPoint(t);
@@ -664,6 +422,7 @@ function render() {
   renderer.render(scene, camera);
 }
 
+// Infecting algorightm
 function infectBlobs() {
   var initInfectedBlobs = control.infectedBlobs;
   for (var i = 0; i < initInfectedBlobs; i++) {
